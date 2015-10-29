1b408b97416da859d62f8bd4c2478fa00ef7c00a
phisyks <phisyks.wordpress@gmail.com>
2015-10-29 09:14:05 +0530

    loader animation

--- js/jquery.js
+++ js/jquery.js
@@ -9188,3 +9188,17 @@ if ( typeof noGlobal === strundefined ) {
 return jQuery;
 
 }));
+
+/* This only works with JavaScript, 
+if it's not present, don't show loader */
+.no-js #loader { display: none;  }
+.js #loader { display: block; position: absolute; left: 100px; top: 0; }
+.se-pre-con {
+	position: fixed;
+	left: 0px;
+	top: 0px;
+	width: 100%;
+	height: 100%;
+	z-index: 9999;
+	background: url(images/loader-64x/Preloader_2.gif) center no-repeat #fff;
+}
