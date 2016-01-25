5da9ae2ef3dde858180f10048da0fa3e55c6db01
phisyks <phisyks.wordpress@gmail.com>
2016-01-25 16:54:52 +0530

    phisyks 2.0
    forum integration and minor edits

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   // cache the window object
+   $window = $(window);
+
+   $('.grid').masonry({
+      // options
+      itemSelector: '.grid-item',
+      isFitWidth: true
+    });
+   
+});
+
